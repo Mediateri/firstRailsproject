3ca2ac4c8ebf20d4b7ef1d68fa47cbe704ee9f5a
Mediateri <ntawukuriryayomeddy1@gmail.com>
2019-08-01 11:09:15 +0200

    Rails

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
     validates :name, :content, presence: true
-    validates :content, length: { maximum: 140 }
+    validates :content, length: { minimum: 140 }
 
 end
